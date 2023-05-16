12604ce53820ce7ee2e9feb55cd8cadfb2f832d6
Kubra Celik <kubracelikce@gmail.com>
2023-05-17 01:25:25 +0300

    add global exception handler

--- inventory-service/src/main/java/com/kodlamaio/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/InventoryServiceApplication.java
@@ -1,12 +1,13 @@
 package com.kodlamaio.inventoryservice;
 
 
+import com.kodlamaio.commonpackage.utils.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Inventory.ServiceBasePackage})
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
