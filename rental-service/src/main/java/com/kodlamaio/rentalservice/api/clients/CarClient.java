cf27b3ed9bc39f52a1d363b9602b708b897a5afa
Kubra Celik <kubracelikce@gmail.com>
2023-05-19 23:49:37 +0300

    add fault tolerance limit

--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClient.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClient.java
@@ -1,6 +1,7 @@
 package com.kodlamaio.rentalservice.api.clients;
 
 import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
 public interface CarClient {
     @GetMapping(value = "/api/cars/check-car-available/{carId}")
+    @Retry(name = "check-car-availability")
     ClientResponse checkIfCarAvailable(@PathVariable UUID carId);
 
 }
